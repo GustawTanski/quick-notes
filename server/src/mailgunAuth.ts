87103b5340113e6bc5f5dc5a61176d7c46ef02bb
Damian Borowy <tymooteuusz@gmail.com>
2019-09-16 22:07:46 +0200

    Removed files containing sensitive data

--- server/src/mailgunAuth.ts
+++
@@ -1,8 +0,0 @@
-const auth = {
-    auth: {
-        api_key: "520c8958f6e3becd234c7f9ac4b6084d-c27bf672-9b265592",
-        domain: "sandbox96e63c509514409db69996c0ae6e4544.mailgun.org"
-    }
-};
-
-export default auth;
