0436419651c8bbfcbbe8a040ec06fc11c5998bfe
BartoszB <bartosz.burdalski@gmail.com>
2019-09-19 14:44:55 +0200

    Small changes

--- src/components/Router/index.js
+++ src/components/Router/index.js
@@ -1,31 +1,40 @@
 import Controller from "../../utils/Controller";
-import View from "./View";
 import Model from "./Model";
 
 export default class Router extends Controller {
-	constructor(node, route) {
+	constructor(node) {
 		super(node);
 		this.model = new Model();
-		this.view = new View(route);
 		this.hrefHandler = this.hrefHandler.bind(this);
-
 		this.login = new LoginForm(node);
 		this.register = new RegisterForm(node);
 		this.noteCreator = new NoteCreator(node);
-
 		this.routes = {
-			login: "thislogin",
-			register: "this.register",
-			noteCreator: "this.noteCreator"
+			login: this.login,
+			register: this.register,
+			noteCreator: this.noteCreator
 		};
+		this.currentRoute = null;
 		this.hrefHandler();
 	}
-	onRouteChange(newRoute) {
-		if (typeof newRoute != "string") {
-			return console.log("Podaj poprawny route (jako string).");
+	onRouteChange(route) {
+		if (typeof route != "string") {
+			return console.log("This parameter should be string.");
 		}
-		let route = this.routes[newRoute].view.element;
-		this.view.changeRoute(route);
+		let newRoute = this.routes[route];
+		let isSameRoute = this.isSameRoute(newRoute);
+		if (this.currentRoute != null && !isSameRoute) this.currentRoute.unmount();
+		this.mountComponent(newRoute, isSameRoute);
+	}
+	mountComponent(newRoute, isSameRoute) {
+		if (!isSameRoute) {
+			this.currentRoute = newRoute;
+			this.currentRoute.mount();
+		}
+	}
+	isSameRoute(toCheck) {
+		if (this.currentRoute == toCheck) return true;
+		else return false;
 	}
 	setListeners() {
 		window.addEventListener("hashchange", this.hrefHandler);
--- src/utils/View.js
+++ src/utils/View.js
@@ -1,9 +1,27 @@
 export default class View {
 	constructor() {
 		this.children = [];
+		this._rendered = false;
 	}
+
+	set children(children) {
+		if (
+			children instanceof Array &&
+			children.every(child => child instanceof HTMLElement)
+		)
+			this._children = children;
+		else throw TypeError("Wrong children type. Should be Array<HTMLElement>");
+	}
+
+	get children() {
+		return this._children;
+	}
+
 	render() {
-		this.children.forEach(child => this.element.appendChild(child));
+		if (!this._rendered) {
+			this.children.forEach(child => this.element.appendChild(child));
+			this._rendered = true;
+		}
 		return this.element;
 	}
 }
