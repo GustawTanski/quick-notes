f318d4c3515d5a70db2c1c7a701677b19c2e31d1
Damian Borowy <tymooteuusz@gmail.com>
2019-09-19 20:34:43 +0200

    Added middleware for decoding headers on /login and /register
    Thanks to Patryk Wydmuch again, I'm just pasting his code

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express, { Router, NextFunction, Request, Response } from "express";
 import userController from "../controllers/userController";
 
 const router: Router = express.Router();
@@ -13,5 +13,13 @@ router.get("/forgot/", userController.renderInputEmailForm);
 router.post("/forgot/", userController.passwordRecoveryEmail);
 router.get("/recover", userController.renderPasswordRecoveryForm);
 router.post("/recover", userController.updateUserPassword);
+
+router.use(["/login", "/register"], (req: Request, res: Response, next: NextFunction) => {
+	const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
+	const [username, password] = new Buffer(b64auth, "base64").toString().split(":");
+    req.body = { email: username, password };
+    
+	return next();
+});
 
 export default router;
