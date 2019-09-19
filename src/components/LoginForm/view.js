5d555fec0a2140bb76b58a74766c96a035e598bc
Izabela Popiel <izabelapopiel1@gmail.com>
2019-09-19 10:54:14 +0200

    input requirements added

--- src/components/LoginForm/view.js
+++ src/components/LoginForm/view.js
@@ -9,13 +9,13 @@ export default class LoginFormView extends View {
 		const emailGroup = document.createElement("div");
 		emailGroup.innerHTML = `
 		<label for="emailInput">Email</label>
-		<input placeholder='email' type ="email" id="emailInput" class="form-control">`;
+		<input placeholder='email' type ="email" maxlength="255" required id="emailInput" class="form-control">`;
 		this.emailInput = emailGroup.querySelector(".form-control");
 
 		const passwordGroup = document.createElement("div");
 		passwordGroup.innerHTML = `
 		<label for="passwordInput">Password</label>
-		<input placeholder='password' type ="password" id="passwordInput" class="form-control">`;
+		<input placeholder='password' type ="password" pattern=".{5,}" required title="5 characters minimum" maxlength="255" id="passwordInput" class="form-control">`;
 		this.passwordInput = passwordGroup.querySelector(".form-control");
 
 		const submitGroup = document.createElement("div");
