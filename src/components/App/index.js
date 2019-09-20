8359a07e6489d31ac4d2bd68acb4c20786ad0d36
Krzysztof <dydakkrzysztof@gmail.com>
2019-09-20 14:26:32 +0200

    Merge remote-tracking branch 'origin/Note' into NoteCreator

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,15 @@
 import Controller from "../../utils/Controller";
 import TopBar from "../TopBar";
 import SpinnerButton from "../SpinnerButton";
-import NoteCreator from "../NoteCreator/index";
 
 export default class App extends Controller {
 	constructor(node) {
 		super(node);
+		this.note = new Note(node, {
+			color: "red",
+			title: "blue",
+			content: "sersfs"
+		});
 		this.topBar = new TopBar(node);
 		this.spinner = new SpinnerButton(node);
 		this.note = new NoteCreator(node);
