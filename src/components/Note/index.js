88c2ffe3e067e9956ac8438f26ffb7203ba7509d
deBroglie <tanski.gustaw@gmail.com>
2019-09-22 14:31:49 +0200

    Note can be erased

--- src/components/Note/index.js
+++ src/components/Note/index.js
@@ -1,11 +1,42 @@
 import Controller from "../../utils/Controller";
 import Model from "./model";
 import View from "./view";
+import RequestManager from "../../utils/RequestManager";
 
 export default class Note extends Controller {
-	constructor(node, config) {
+	constructor(node, config, onDelete) {
 		super(node, config);
 		this.model = new Model(config);
 		this.view = new View(this.model.config);
+		this._onCrossButtonClicked = this._onCrossButtonClicked.bind(this);
+		this._onDelete = onDelete;
+	}
+
+	async _onCrossButtonClicked() {
+		const { config } = this.model;
+		console.log(config);
+		if (confirm("Are you sure?")) {
+			try {
+				const res = await RequestManager.deleteNote(
+					config.authorId,
+					config.noteId
+				);
+				if (typeof this._onDelete == "function") this._onDelete();
+				console.log("Deleted!");
+			} catch (error) {
+				alert("Something went wrong! Try again.");
+			}
+		}
+	}
+
+	setListeners() {
+		this.view.crossButton.addEventListener("click", this._onCrossButtonClicked);
+	}
+
+	removeListeners() {
+		this.view.crossButton.removeEventListener(
+			"click",
+			this._onCrossButtonClicked
+		);
 	}
 }
--- src/components/Note/model.js
+++ src/components/Note/model.js
@@ -5,17 +5,30 @@ export default class NoteModel {
 	}
 
 	set config(config) {
-		if ("color" in config && "title" in config && "content" in config) {
-			if (!possibleColors.some(color => color == config.color))
-				throw Error("Wrong color!");
+		if (this._hasConfigNeededProps(config)) {
+			if (!this._havePropsProperTypes(config))
+				throw Error("Some props have wrong type!");
 			this._config = config;
 		} else {
-			throw TypeError("Wrong config param!");
+			throw TypeError("Some needed props are missing!");
 		}
 	}
 
 	get config() {
 		return this._config;
+	}
+
+	_hasConfigNeededProps(config) {
+		const propNames = ["color", "title", "content", "authorId", "noteId"];
+		return propNames.every(propName => propName in config);
+	}
+
+	_havePropsProperTypes(config) {
+		const isColorGood = possibleColors.some(color => color == config.color);
+		const strings = ["title", "content", "authorId", "noteId"];
+		return (
+			isColorGood && strings.every(string => typeof config[string] == "string")
+		);
 	}
 
 	setConfig(config) {
